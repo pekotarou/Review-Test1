207d688de6882165f11c6f032f585ba3ba7d2c18
hangyo_non <peco8686@yahoo.co.jp>
2026-02-16 11:21:48 +0900

    cssファイル作成

---
+++ src/resources/views/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/create.css') }}" />
+@endsection
+
+@section('navi')
+<!--
+    <nav>
+        <ul class="header-nav">
+           <li class="header-nav__item">
+            
+            <a class="header-nav__link" href="/categories">カテゴリ一覧</a> 
+           </li>
+         </ul>
+       </nav>
+-->
+@endsection
